b555f925be297e2f8751add5a616a602de051a16
anderson.conceicao <anderson.conceicao@b2wdigital.com>
2021-03-24 12:16:36 -0300

    Add estrutura componente welcome

--- src/components/Welcome/index.js
+++ src/components/Welcome/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import * as S from './styles';
 
-const Welcome = () => {
+const Welcome = ({ img, title, subtitle }) => (
+  <S.Wrapper>
+    <S.Heading>
+      <S.Title>{title}</S.Title>
+      <S.Subtitle>{subtitle}</S.Subtitle>
+    </S.Heading>
+    <S.Image src={img} role="img" aria-label={title} />
+  </S.Wrapper>
+);
 
-  return (
-    <>
-      <h1>CRA Storybook</h1>
-    </>
-  )
-}
+Welcome.propTypes = {
+  img: PropTypes.string,
+  title: PropTypes.string.isRequired,
+  subtitle: PropTypes.string.isRequired,
+};
 
 export default Welcome;
